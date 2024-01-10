c921181ee369c9e02a61341088c30e26a985fc69
gayagaya3r1 <152364109+gayagaya3r1@users.noreply.github.com>
2024-01-10 12:18:40 +0530

    Update practice.sh
    all my changes

--- practice.sh
+++ practice.sh
@@ -4,3 +4,11 @@ person1=seetha
 person2=ameetha
 echo "hi $person1"
 echo "hi $person2"
+date=$(date)
+echo "today's $date"
+echo "enter username"
+read -s USERNAME
+echo "$USERNAME"
+echo "enter password"
+read -s PASSWORD
+echo "$PASSWORD"
